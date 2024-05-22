253b8e189df67668f454915a7d7444e5ce2b22ba
ryanny <ryannymromero@gmail.com>
2024-05-22 03:42:18 -0800

    Make the update cart quantity interactive

--- scripts/data/cart.js
+++ scripts/data/cart.js
@@ -36,7 +36,43 @@ class Cart {
     return this.cartItems.reduce((acc, cur) => acc + cur.quantity, 0);
   }
 
-  
+  updateQuantity(cartItemsEl) {
+    
+    let valid = true;
+
+    cartItemsEl.forEach(el => {
+
+      // TODO: Clear Format, Add a choice when deleting the product
+      const inputValue = parseInt(el.value);
+      const { productId } = el.dataset;
+
+      if(this.isInvalidInput(inputValue)) {
+        valid = false;
+        return;
+      } else if(inputValue === 0) {
+        alert('The Item will be removed');
+        document.querySelector(`.product-${productId}`).remove();
+        this.cartItems = this.cartItems.filter(cartItem => cartItem.productId !== productId);
+        this.saveToStorage();
+      }
+      else {
+        const matchingProduct = this.cartItems.find(cartItem => productId === cartItem.productId);
+        matchingProduct.quantity = inputValue;
+      }      
+      this.saveToStorage();
+    });
+
+    if(valid) {
+      alert('cart Updated!');
+    } else {
+      alert('Something went wrong!');
+    }
+  }
+
+  isInvalidInput(inputValue) {
+    return inputValue === '' || isNaN(inputValue);
+  }
+ 
 }
 
 export const cart = new Cart();
--- scripts/js-page/cartPage.js
+++ scripts/js-page/cartPage.js
@@ -1,11 +1,10 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
-import { getDiscountedPrice, formatCurrency} from "../utils/money.js"
+import { getDiscountedPrice, formatCurrency } from "../utils/money.js";
 import { displayCartQuantity } from "../utils/reusableComp.js";
 
 function renderCartHTML() {
-
-  // Dislay Cart Quanttiy
+  // Display Cart Quantity
   displayCartQuantity();
 
   const cartHTML = cart.cartItems.map(cartItem => {
@@ -13,22 +12,31 @@ function renderCartHTML() {
     const subTotal = (getDiscountedPrice(matchingProduct) * cartItem.quantity).toFixed(2);
 
     return `
-    <div class="product text-xs grid grid-cols-3 items-center justify-items-center md:justify-items-start md:grid-cols-4">
+    <div class="product product-${matchingProduct.id} text-xs grid grid-cols-3 items-center justify-items-center md:justify-items-start md:grid-cols-4">
       <div class="product flex items-center gap-2 relative">
         <img src="../${matchingProduct.image}" class="product-img h-16 w-16" alt="">
         <p class="product-name">${matchingProduct.name}</p>
-        <p class="product-price absolute md:static md:hidden" style="left: 70px; bottom: -7px;">$${formatCurrency(matchingProduct.priceCents)}</p>
+        <p class="product-price absolute md:static md:hidden " style="left: 70px; bottom: -7px;">$${formatCurrency(matchingProduct.priceCents)}</p>
       </div>
       <p class="product-price hidden md:block">$${getDiscountedPrice(matchingProduct)}</p>
-      <input type="number" min="1" value="1" class="w-11 sm:w-14 border border-gray-400 p-2 outline-gray-400 justify-self-end sm:justify-self-center md:justify-self-star">
-      <p class="justify-self-end sm:justify-self-center md:justify-self-star">${subTotal}</p> 
+      <input type="text" value="${cartItem.quantity}" class="p-2 border border-gray-400 outline-none justify-self-end sm:justify-self-center md:justify-self-start product-input" style="width: 48px;" data-product-id="${matchingProduct.id}">
+      <p class="justify-self-end sm:justify-self-center md:justify-self-start">${subTotal}</p> 
     </div>
-    `
-  });
-
+    `;
+  }).join('');
 
   const cartItemWrapper = document.querySelector('.cart-item-wrapper');
-  cartItemWrapper.innerHTML += cartHTML;
+  cartItemWrapper.innerHTML = cartHTML;
+
+  const updateCartBtn = document.querySelector('.update-cart-btn');
+  updateCartBtn.removeEventListener('click', updateCartButtonClickHandler);
+  updateCartBtn.addEventListener('click', updateCartButtonClickHandler);
+}
+
+function updateCartButtonClickHandler() {
+  const productInputEls = document.querySelectorAll('.product-input');
+  cart.updateQuantity(productInputEls);
+  renderCartHTML();
 }
 
 renderCartHTML();
