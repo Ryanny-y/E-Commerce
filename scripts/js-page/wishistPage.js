4904c918240a0a35955e873923805cdd44cff1ee
ryanny <ryannymromero@gmail.com>
2024-05-24 22:23:14 -0800

    Try to fix the imgs

--- scripts/js-page/wishistPage.js
+++ scripts/js-page/wishistPage.js
@@ -76,10 +76,10 @@ function renderJustForYou() {
         <p class="sale bg-p-red text-white py-1 px-3 absolute top-0 left-0 text-xs text-center rounded-sm">-${getDiscount(product.discount)}%</p>
         <i class="wishlist-btn fa-regular fa-heart h-transition hover:text-p-red block absolute top-2 right-2" data-product-id="${product.id}"></i>
         <i class="view-product-btn fa-regular fa-eye h-transition hover:text-p-red block absolute top-10 right-2" data-product-id="${product.id}"></i>
-        <img class="h-full w-full px-10 py-8" src="/${product.image}" alt="">
+        <img class="h-full w-full px-10 py-8" src="../${product.image}" alt="">
         <button class="bg-black text-xs md:text-sm rounded-t-md absolute bottom-0 right-0 left-0 h-10 text-white add-to-cart-btn h-transition hover:bg-p-red" data-product-id="${product.id}">
           <i class="fa-solid fa-cart-shopping h-transition mr-0 md:mr-2 md:text-sm hover:text-p-red"></i>
-         Add to Cart</button>
+        Add to Cart</button>
       </div>
       
       <div class="product-description flex flex-col gap-1">
@@ -91,7 +91,7 @@ function renderJustForYou() {
         </span>
 
         <span class="ratings flex items-center h-4">
-          <img class="rating-star h-full" src="/${product.getRatingStar()}" alt="">
+          <img class="rating-star h-full" src="../${product.getRatingStar()}" alt="">
           <p>(${product.getRatingCount()})</p>
         </span>
       </div>
